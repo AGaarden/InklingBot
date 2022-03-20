082e4e7aa3ee81dfd1450be56201ebc9fb8665b7
Sander Gaarden <sandergaarden@gmail.com>
2022-03-20 02:05:31 +0100

    Added InitializeWordLists, this returns map with lists of all highlighted words. Updated CheckForHighlights to work with this.

--- Utility_Functions/highlightFunctions.js
+++ Utility_Functions/highlightFunctions.js
@@ -2,35 +2,54 @@ const rw = require('./rwFunctions.js');
 const fs = require('fs');
 
 module.exports = {
+  InitializeWordLists,
   CheckForHighlights,
   IdForWords,
 };
 
+function InitializeWordLists() {
+  const output = new Map();
+
+  const serverIds = fs.readdirSync('./Highlights');
+
+  serverIds.forEach(server => {
+    const serverWords = fs.readdirSync(`./Highlights/${server}`);
+    for(let i = 0; i < serverWords.length; i++) {
+      serverWords[i] = serverWords[i].slice(0, -4);
+    }
+
+    output.set(server, serverWords);
+  });
+
+  return output;
+}
+
 // This checks whether a server has someone with a given word highlighted
 // This returns an array of words that should be highlighted
-function CheckForHighlights(event) {
+function CheckForHighlights(message, listBuffer) {
   // Sanitise input for only letters + whitespace and split into separate array
-  const messageSent = event.content.replace(/[^a-z\s]/gi, '');
+  const messageSent = message.content.replace(/[^a-z\s]/gi, '');
   const wordArray = messageSent.toLowerCase().split(/ +/);
 
   // Make array of words from all file names in server folder
-  const wordFiles = fs.readdirSync(`./Highlights/${event.guild.id}`);
+  const savedWordList = listBuffer.get(message.guild.id);
 
   // For loop checking wordArray against wordFiles
   const foundWords = [];
   for(let i = 0; i < wordArray.length; i++) {
-    for(let j = 0; j < wordFiles.length; j++) {
+    for(let j = 0; j < savedWordList.length; j++) {
       // If it is a txt file, make the word not a txt file
       console.log(wordArray[i]);
-      console.log(wordFiles[j]);
+      console.log(savedWordList[j]);
 
-      let wordFromFile = wordFiles[j];
-      if(wordFromFile.slice(-4) == '.txt') wordFromFile = wordFiles[j].slice(0, -4);
-
-      console.log(wordFromFile);
+      console.log(savedWordList[j]);
 
       // If a word from message fits a word from file, do thing
-      if(wordArray[i] == wordFromFile) foundWords.push(wordFromFile);
+      if(wordArray[i] == savedWordList[j]) {
+        foundWords.push(savedWordList[j]); // Push the found word to output list
+        savedWordList.splice(j, 1); // Remove the possible word to find from the list
+        break; // Break out of loop so it does not check through the whole list for the same word
+      }
     }
   }
 

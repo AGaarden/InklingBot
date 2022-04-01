a64e015665d0b5cc13b78c816ed87c96ab629b0a
Sander Gaarden <sandergaarden@gmail.com>
2022-04-01 14:00:28 +0200

    addHighlight no longer adds user to an already added highlight.

--- Utility_Functions/Highlight_Functions/addHighlight.js
+++ Utility_Functions/Highlight_Functions/addHighlight.js
@@ -1,4 +1,5 @@
 const rw = require('../rwFunctions.js');
+const fs = require('fs');
 
 module.exports = {
   name: 'add',
@@ -10,14 +11,24 @@ module.exports = {
   execute(message, commandArgs, wordList) {
     // Join all arguments in a string by spaces, replace potential newlines
     const highlight = commandArgs.join(' ').replace(/\r\n|\r|\n/g, ' ');
+    const highlightFilePath = `./Highlights/${message.guild.id}/${highlight.toLowerCase()}.txt`;
 
+    // If the highlight has any of the characters in the regex, don't allow it
     if(highlight.match(/[\\/:*?"<>|]/g)) {
       message.channel.send('Please change your highlight. I cannot handle these characters: \\ / : * ? " < > |');
       return;
     }
 
+    // Check whether the person already has this highlighted, return if so
+    if(fs.existsSync(highlightFilePath)) {
+      if(rw.ReadList(highlightFilePath).includes(message.author.id)) {
+        message.channel.send('You already have this as a highlight.');
+        return;
+      }
+    }
+
     // Add the user that highlighted the word to the given file
-    rw.WriteLineToList(`./Highlights/${message.guild.id}/${highlight.toLowerCase()}`, message.author.id);
+    rw.WriteLineToList(highlightFilePath, message.author.id);
 
     // Add to the wordlist in lower case
     wordList.get(message.guild.id).push(highlight.toLowerCase());
